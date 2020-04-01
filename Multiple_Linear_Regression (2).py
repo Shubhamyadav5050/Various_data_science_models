60c55a75c4621b07ada7d6ae39bdad572ac0bb21
Shubhamyadav5050 <62754209+Shubhamyadav5050@users.noreply.github.com>
2020-04-01 17:30:55 +0530

    Update Multiple_Linear_Regression (2).py

--- Multiple_Linear_Regression (2).py
+++ Multiple_Linear_Regression (2).py
@@ -26,13 +26,7 @@ X=X[:, 1:]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
  
-# Feature Scaling  
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)""" 
+
 
 
 #fitting multiple linear regression to training set
